55761653fe5845fd57090f9d8d5bb8ba783e55b5
Afnan Attar <afnan.attar74@gmail.com>
2022-11-02 19:59:33 +0530

    Huffman Coding

--- DDA/2. Huffman_Coding.cpp
+++ DDA/2. Huffman_Coding.cpp
@@ -2,28 +2,61 @@
 
 using namespace std;
 
-class letter{
-	public:
-	char c;
+struct MinHeapNode{
+	char data;
 	int freq;
+	MinHeapNode* left, *right;
+	MinHeapNode(char data, int freq){
+		left=right=nullptr;
+		this->data = data;
+		this->freq = freq;
+	}
 };
 
-class myComparator{
-	public:
-	int operator(const letter& l1, const letter& l2) return l1.freq > l2.freq;
+
+void printCodes(struct MinHeapNode* root, string str){
+	if(root == nullptr){
+		return;
+	}
+	if(root->data != '$'){
+		cout << root->data << ": " << str << endl;
+	}
+	printCodes(root->left, str + "0");
+	printCodes(root->right, str + "1");
+}
+
+struct compare{
+	bool operator()(MinHeapNode* a, MinHeapNode* b){
+		return (a->freq > b->freq);
+	}
 };
+
+void HuffmanCode(char data[], int freq[], int size){
+	struct 	MinHeapNode *left, *right, *temp;
+	
+	priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
+	
+	for(int i = 0; i < size; i++){
+		minHeap.push(new MinHeapNode(data[i], freq[i]));
+	}
+	
+	while(minHeap.size() != 1){
+		left = minHeap.top();
+		minHeap.pop();
+		right = minHeap.top();
+		minHeap.pop();
+		temp = new MinHeapNode('$', left->freq + right->freq);
+		temp->left = left;
+		temp->right = right;
+		minHeap.push(temp);
+	}
+	printCodes(minHeap.top(), "");
+}
+
 
 int main(){
-    string s = "ABCDEACEDBEE";
-	int h[26] = {0};
-	for(char c : s){
-		h[c - 'A'] += 1;
-	}
-	priority_queue<letter, vector<letter>, myComparator> h;
-	
-	for(int i = 0; i < 26; i++){
-		if(h[i] > 0) h.push_back('A' + i, h[i])
-	
-	
-    return 0;
+	char data[] = {'A', 'B', 'C', 'D'};
+	int freq[] = {23,12,34,10};
+	HuffmanCode(data, freq, 4);
+	return 0;
 }
