bc158481820657ed7803003f5a6a7f09255b9bde
Afnan Attar <afnan.attar74@gmail.com>
2022-11-02 13:29:35 +0530

    ML Completed

--- DDA/2. Huffman_Coding.cpp
+++ DDA/2. Huffman_Coding.cpp
@@ -1,12 +1,29 @@
-#include<iostream>
-#include<string>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+class letter{
+	public:
+	char c;
+	int freq;
+};
+
+class myComparator{
+	public:
+	int operator(const letter& l1, const letter& l2) return l1.freq > l2.freq;
+};
+
 int main(){
     string s = "ABCDEACEDBEE";
-    //1. Count Frequency of Each letter
-    //2. Sort in increasing order of frequency
-    
+	int h[26] = {0};
+	for(char c : s){
+		h[c - 'A'] += 1;
+	}
+	priority_queue<letter, vector<letter>, myComparator> h;
+	
+	for(int i = 0; i < 26; i++){
+		if(h[i] > 0) h.push_back('A' + i, h[i])
+	
+	
     return 0;
 }
