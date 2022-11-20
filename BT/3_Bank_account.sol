ddd659556f717cfc4e8dd02c65319ae86aaf0b99
Afnan Attar <afnan.attar74@gmail.com>
2022-11-20 16:11:08 +0530

    Simpler

--- BT/3_Bank_account.sol
+++ BT/3_Bank_account.sol
@@ -1,45 +1,38 @@
 pragma solidity >= 0.7.0;
-// Write  a  smart  contract  on  a  test  network,  for  Bank  account  of  a  customer  for  // following operations: Deposit money | Withdraw Money | Show balance
-
-contract banking{
+// Write  a  smart  contract  on  a  test  network,  for  Bank  account  of  a  customer  for
+  // following operations: Deposit money | Withdraw Money | Show balance
+contract Bank{
     mapping(address => uint) public user_account;
-    mapping(address=>bool) public user_exists;
+    mapping(address => bool) public user_exist;
 
-    function create_account() public payable returns (string memory){
-        require(user_exists[msg.sender] == false, 'Account already created');
-        if(msg.value == 0){
-            user_account[msg.sender] = 0;
-            user_exists[msg.sender] = true;
-            return "Account Created";
-        }
-        require(user_exists[msg.sender] == false, "Account already created");
+    function create_account() public payable returns(string memory){
+        require(user_exist[msg.sender] == false, "Account Already created!");
         user_account[msg.sender] = msg.value;
-        user_exists[msg.sender] = true;
+        user_exist[msg.sender] = true;
         return "Account created";
     }
 
-    function deposit(uint amount) public payable returns (string memory){
-        require(user_exists[msg.sender] == true, "Account not created");
-        require(amount > 0, "Value for deposit is Zero");
-        user_account[msg.sender] = user_account[msg.sender] + amount;
-        return "Deposisted Successfully";
+    function deposit(uint amount) public payable returns(string memory){
+        require(user_exist[msg.sender] == true, "Account not created!");
+        require(amount > 0, "Amount should be greater than 0");
+        user_account[msg.sender] += amount;
+        return "Amount deposisted sucessfully";
     }
 
-    function withdraw(uint amount) public payable returns (string memory){
-        require(user_account[msg.sender] > amount, "Insufficient Balance");
-        require(user_exists[msg.sender] == true, "Account not created");
-        require(amount > 0, "Value for withdraw is Zero");
-        user_account[msg.sender] = user_account[msg.sender] - amount;
-        return "Withdrawn Successfully";
+    function withdraw(uint amount) public payable returns(string memory){
+        require(user_exist[msg.sender] == true, "Account not created!");
+        require(amount > 0, "Amount should be greater than 0");
+        require(user_account[msg.sender] >= amount, "Amount is greater than money deposisted");
+        user_account[msg.sender] -= amount;
+        return "Amount withdrawn sucessfully";    
     }
 
-
-    function user_balance() public view returns(uint){
+    function account_balance() public view returns(uint){
         return user_account[msg.sender];
     }
-
+    
     function account_exists() public view returns(bool){
-        return user_exists[msg.sender];
+        return user_exist[msg.sender];
     }
 
 }
