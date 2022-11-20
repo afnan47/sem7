1efb010283804e0ef746991531b5386fff731acf
Afnan Attar <afnan.attar74@gmail.com>
2022-11-20 22:20:33 +0530

    Easier

--- DAA/3_Fractional_Knapsack.py
+++ DAA/3_Fractional_Knapsack.py
@@ -5,10 +5,12 @@ price = 0
 arr = sorted(arr, key = lambda x : x[0] / x[1], reverse = True)
 
 for i in range(len(arr)):
-	if(arr[i][1] > w):
-		price += (arr[i][1] -(arr[i][1]-w))*(arr[i][0] / arr[i][1])
+	itemWt = arr[i][1]
+	itemP = arr[i][0]
+	if(itemWt > w):
+		price += w*(itemP / itemWt)
 	else:
-		price += arr[i][0]	
-		w -= arr[i][1]
+		price += itemP	
+		w -= itemWt
 
 print(price)
